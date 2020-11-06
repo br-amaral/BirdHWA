c6ea98767b191e607df460844ee693efd2d00001
Kyle Husmann <kyle.husmann@gmail.com>
2020-11-05 21:36:40 -0500

    Get speciesList from online source

--- combineData.R
+++ combineData.R
@@ -1,26 +1,54 @@
 library(tidyverse)
 library(fs)
+library(rvest)
 
 STATE_DATA_PATH <- "data/src/StateData"
 INFESTATIONS_PATH <- "data/src/infestations.rds"
-SPECIES_LIST_PATH <- "data/src/SspList.csv"
 WEATHER_PATH <- "data/src/weather.csv"
+SPECIES_TABLE_URL <- "https://www.pwrc.usgs.gov/BBl/manual/speclist.cfm"
+
+speciesList <- read_html(SPECIES_TABLE_URL) %>%
+  html_nodes("table") %>%
+  html_table(fill = T) %>%
+  flatten_df() %>%
+  select(SpeciesId = `Species Number`,
+         SpeciesCode = `Alpha Code`,
+         SpeciesName = `Common Name`,
+         SpeciesSciName = `Scientific Name`)
+  
+
+weather <- read_csv(WEATHER_PATH, col_types = cols_only(
+  StateNum = col_integer(),
+  Route = col_integer(),
+  Year = col_integer(),
+  ObsN = col_integer()
+)) %>%
+  rename(ObserverId = ObsN)
 
 infestations <- read_rds(INFESTATIONS_PATH) %>%
-  extract(RouteId, c("StateNum", "Route"), "(..)(...)") %>%
+  extract(RouteId, c("StateNum", "Route"), "(..)(...)", convert=T) %>%
   select(-Infested) %>%
-  pivot_longer(`2018`:`1951`, names_to = "Year", values_to = "Infested") %>%
+  pivot_longer(`2018`:`1951`, names_to = "Year", values_to = "Infested",
+               names_transform = list(Year = as.integer),
+               values_ptypes = list(Infested = logical())) %>%
   select(StateNum, Route, Year, Infested)
 
 stateData <- STATE_DATA_PATH %>%
   dir_ls() %>%
-  map(read_csv, col_types = "ccccccciiiiiii") %>%
+  map(read_csv, col_types = cols_only(
+    Year = col_integer(),
+    StateNum = col_integer(),
+    Route = col_integer(),
+    RPID = col_integer(),
+    AOU = col_integer(),
+    SpeciesTotal = col_integer()
+  )) %>%
   bind_rows() %>%
-  left_join(infestations, by = c("StateNum", "Route", "Year"))
+  rename(ObsType = RPID, SpeciesId = AOU)
 
+BirdHWA <- stateData %>%
+  left_join(infestations, by = c("StateNum", "Route", "Year")) %>%
+  left_join(speciesList, by = c("SpeciesId")) %>%
+  left_join(weather, by = c("StateNum", "Route", "Year"))
 
-speciesList <- read_csv(SPECIES_LIST_PATH)
-
-weather <- read_csv(WEATHER_PATH)
-
-  
+save(BirdHWA, file = 'data/BirdHWA.rda') 
