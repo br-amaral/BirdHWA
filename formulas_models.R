a2b99c74d10fc6fcd3be23ee24a3f76062258b52
br-amaral <br-amaral@users.noreply.github.com>
2021-02-08 13:54:24 -0500

    Create formulas_models.R
    Formulas to the INLA model

---
+++ formulas_models.R
@@ -0,0 +1,112 @@
+### with temperature: ##
+## full model
+formula1 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  infoff +
+  yrhwa * infoff +
+  NewObserver +
+  minTemp +
+  minTemp * yrhwa +
+  minTemp * infoff +
+  minTemp * infoff * yrhwa +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
+
+## minus infoff: no initial change, broken stick with 2 connected lines
+formula2 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  yrhwa * infoff +
+  NewObserver +
+  minTemp +
+  minTemp * yrhwa +
+  minTemp * infoff * yrhwa +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
+
+## minus yrhwa * infoff: no change in trend - one time effect
+formula3 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  infoff +
+  NewObserver +
+  minTemp +
+  minTemp * yrhwa +
+  minTemp * infoff +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
+
+## modeel in between shgould have temperature times year!
+## only minTemp but no temp interaction - additive effect
+formula4 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  infoff +
+  yrhwa * infoff +
+  NewObserver +
+  minTemp * yrhwa +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
+
+## only minTemp but no interaction - additive effect + only intercept change
+formula5 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  infoff +
+  NewObserver +
+  minTemp * yrhwa +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE)
+
+## only minTemp but no interaction - additive effect + only slope change
+formula6 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  yrhwa * infoff +
+  NewObserver +
+  minTemp * yrhwa +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
+
+### No temperature ###
+## only intercept, yrhwa, NewObservers - null model
+formula7 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  NewObserver +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE) 
+
+## intercept, yrhwa, NewObservers + plus intercept change
+formula8 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  infoff +
+  NewObserver +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE) 
+
+## intercept, yrhwa, NewObservers + plus trend change
+formula9 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  yrhwa * infoff +
+  NewObserver +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE) 
+
+## intercept, yrhwa, NewObservers + intercept and trend change
+formula10 <- SpeciesTotal ~ 1 + 
+  yrhwa + 
+  infoff +
+  yrhwa * infoff +
+  NewObserver +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE) 
+
+
+formulas <- list(formula1,formula2,formula3,formula4,
+                 formula6,formula6,formula7,formula8,
+                 formula9,formula10)
