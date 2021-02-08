c7e0e3af7b3209cda8914612a13992274b7ecd59
br-amaral <br-amaral@users.noreply.github.com>
2021-02-08 14:01:29 -0500

    Create single_sps.R
    Seperate single species in seprate data frames, and add locations where it occurs but it was not detected

---
+++ single_sps.R
@@ -0,0 +1,30 @@
+library(purrr)
+library(dplyr)
+
+# Create individual species data.frames to run the models
+SPECIES_DATA_PATH <- "data/src/sps_list.csv"
+BIRD_FILE_PATH <- "data/BirdHWA_2.rds"
+
+sps_list <- read_csv(SPECIES_DATA_PATH)
+BIRD <- readRDS(BIRD_FILE_PATH)
+
+single_sps <- function(species){
+  ## select a species and keep undetected records --------------------
+  route_range <- BIRD %>% 
+    filter(SpeciesCode == species) %>% 
+    dplyr::select(RouteId) %>% 
+    unique() %>% 
+    arrange()
+  
+  BIRD2 <- BIRD %>% 
+    filter(RouteId %in% as_vector(route_range)) %>% 
+    filter(SpeciesCode == species | is.na(SpeciesCode))
+  
+  filename <- paste("data/species/", species, ".rds",sep= "")
+    
+  write_rds(BIRD2, file = filename)
+  
+}
+
+invisible(lapply(sps_list$SpeciesCode, single_sps))
+
