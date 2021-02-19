698371a7e1264838a656308ae33274d9d9d1383f
br-amaral <br-amaral@users.noreply.github.com>
2021-02-19 10:27:51 -0500

    Update 5_formulasModels.R
    change formulas to have yrhwa plus offsett in the model

--- 5_formulasModels.R
+++ 5_formulasModels.R
@@ -1,106 +1,109 @@
-### with temperature: ##
-## full model
+# MODELS WITH TEMPERATURE:  --------------------
+# full model ----------
 formula1 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
+  year_offset + 
   infoff +
-  yrhwa * infoff +
+  year_offset * infoff +
   NewObserver +
   temp_min_scale +
-  temp_min_scale * yrhwa +
+  temp_min_scale * year_offset +
   temp_min_scale * infoff +
-  temp_min_scale * infoff * yrhwa +
+  temp_min_scale * infoff * year_offset +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
 
-## minus infoff: no initial change, broken stick with 2 connected lines
+# minus infoff: no initial change, broken stick with 2 connected lines ----------
 formula2 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
-  yrhwa * infoff +
+  year_offset + 
+  year_offset * infoff +
   NewObserver +
   temp_min_scale +
-  temp_min_scale * yrhwa +
-  temp_min_scale * infoff * yrhwa +
+  temp_min_scale * year_offset +
+  temp_min_scale * infoff * year_offset +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
 
-## minus yrhwa * infoff: no change in trend - one time effect
+# minus year_offset * infoff: no change in trend - one time effect ----------
 formula3 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
+  year_offset + 
   infoff +
   NewObserver +
   temp_min_scale +
-  temp_min_scale * yrhwa +
+  temp_min_scale * year_offset +
   temp_min_scale * infoff +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
 
-## modeel in between shgould have temperature times year!
-## only temp_min_scale but no temp interaction - additive effect
+# modeel in between shgould have temperature times year!
+# only temp_min_scale but no temp interaction - additive effect ----------
 formula4 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
+  year_offset + 
   infoff +
-  yrhwa * infoff +
+  year_offset * infoff +
   NewObserver +
-  temp_min_scale * yrhwa +
+  temp_min_scale * year_offset +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
 
-## only temp_min_scale but no interaction - additive effect + only intercept change
+# only temp_min_scale but no interaction ----------
+# additive effect + only intercept change
 formula5 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
+  year_offset + 
   infoff +
   NewObserver +
-  temp_min_scale * yrhwa +
+  temp_min_scale * year_offset +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE)
 
-## only temp_min_scale but no interaction - additive effect + only slope change
+## only temp_min_scale but no interaction ----------
+# additive effect + only slope change
 formula6 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
-  yrhwa * infoff +
+  year_offset + 
+  year_offset * infoff +
   NewObserver +
-  temp_min_scale * yrhwa +
+  temp_min_scale * year_offset +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
 
-### No temperature ###
-## only intercept, yrhwa, NewObservers - null model
+# MODELS WITH NO TEMPERATURE:  --------------------
+# null model ----------
+# only intercept, year_offset, NewObservers
 formula7 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
+  year_offset + 
   NewObserver +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE) 
 
-## intercept, yrhwa, NewObservers + plus intercept change
+## intercept, year_offset, NewObservers + plus intercept change ----------
 formula8 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
+  year_offset + 
   infoff +
   NewObserver +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE) 
 
-## intercept, yrhwa, NewObservers + plus trend change
+## intercept, year_offset, NewObservers + plus trend change ----------
 formula9 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
-  yrhwa * infoff +
+  year_offset + 
+  year_offset * infoff +
   NewObserver +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
   f(hexID, model="bym", graph=hex.adj, constr=TRUE) 
 
-## intercept, yrhwa, NewObservers + intercept and trend change
+## intercept, year_offset, NewObservers + intercept and trend change ----------
 formula10 <- SpeciesTotal ~ 1 + 
-  yrhwa + 
+  year_offset + 
   infoff +
-  yrhwa * infoff +
+  year_offset * infoff +
   NewObserver +
   f(ObserverRoute, model="iid") + 
   f(Year, model="iid") +
