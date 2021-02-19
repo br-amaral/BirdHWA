22f8bf1ed960351b72ad337fd8895c2e5ce67557
br-amaral <br-amaral@users.noreply.github.com>
2021-02-19 10:28:31 -0500

    Update 6_model.R
    - use glue instad of paste
    - remove extract parameters from the modeling code

--- 6_model.R
+++ 6_model.R
@@ -1,12 +1,20 @@
+# 6_model
+
+# Input: 
+#        
+# Output: 
+#         
+
 library(INLA)
 library(tidyverse)
+library(glue)
 
 set.seed(10)
 
 SPECIES_DATA_PATH <- "data/src/sps_list.csv"
-source("formulas_models.R")
+source("5_formulasModels.R")
 sps_list <- read_csv(SPECIES_DATA_PATH)
-hex.adj <- paste(getwd(),"/data/hexmap.graph", sep="")
+hex.adj <- paste0(getwd(),"/data/hexmap.graph")
 
 offsets <- seq(2,16,1)
 
@@ -22,18 +30,6 @@ run_model <- function(offset, BIRDx, formula){
   model <- inla(formula, family="poisson", data=BIRDx, 
                 control.predictor=list(compute=TRUE), 
                 control.compute=list(waic=TRUE, dic=TRUE, cpo=TRUE))
-  summary(model)
-  
-  meaninf <- model$summary.fixed[3,1]
-  lowinf <- model$summary.fixed[3,3]
-  highinf <- model$summary.fixed[3,5]
-  
-  meaninfL <- model$summary.fixed[5,1]
-  lowinfL <- model$summary.fixed[5,3]
-  highinfL <- model$summary.fixed[5,5]
-  
-  waic <- model$waic$waic
-  
   return(model)
 }
 
@@ -42,14 +38,14 @@ run_combinations <- function(species){
     formula <- formulas[[i]]
     for(j in 1:length(offsets)){
       off <- offsets[j]
-      SPECIES_MOD_DAT <- paste("data/species/", species, ".rds", sep= "")
+      SPECIES_MOD_DAT <- glue("data/species/{species}.rds")
       BIRDtab <- readRDS(SPECIES_MOD_DAT)
       resu <- run_model(off,BIRDtab,formula)
-      name <- paste(species, "_model", i, "_", off, "yrs", sep= "")
+      name <- glue("{species}_model{i}_{off}yrs")
       assign(name,resu)
       print(name)
-      name2 <- paste("data/models_res/", species, "/", name, ".RDS", sep= "")
-      dir.create(paste("data/models_res/", species, sep=""))
+      name2 <- glue("data/models_res/{species}/{name}.rds", sep= "")
+      dir.create(glue("data/models_res/{species}"))
       saveRDS(object = get(name), file = name2)
       rm(resu)
       rm(BIRDtab)
