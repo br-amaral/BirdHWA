0a02c264ed4b471c73effa5e83c7bc625ba8d034
br-amaral <br-amaral@users.noreply.github.com>
2021-02-27 16:50:32 -0500

    Update 3_filterData.R
    data descrition
    observervation type correction
    add some spaces

--- 3_filterData.R
+++ 3_filterData.R
@@ -1,8 +1,9 @@
 # 3_filterData
 
-# Input: 
-#        
-# Output: 
+# Input: BirdHWA.rds: tibble created by 2_combineData; has information about the birds detected and occasions
+#                 with no detections and temperature in the route.
+#        infestations_2.rds: tibble created by 2_combineData; has informations about the routes, and infestation
+# Output: BirdHWA_2.rds: data frame with all species and occasions, with info of whether and when routes were infested
 #         
 
 library(tidyverse)
@@ -37,15 +38,16 @@ BirdHWA_n3 <- BirdHWA_f2 %>%
   arrange(RouteId,Year)
 
 first_obs <- BirdHWA_n3 %>% 
-  select(RouteId,Year,ObserverId) %>%
+  select(RouteId, Year, ObserverId, ObserverType) %>%
+  arrange(ObserverType) %>% 
   group_by(ObserverId) %>% 
   filter(Year == min(Year)) %>% 
   slice(1) %>%   # takes the first occurrence if there is a tie
   ungroup() %>%
     mutate(NewObserver = T)
-   
+
 BirdHWA_n4 <- left_join(BirdHWA_n3,first_obs,
-                        by= c('RouteId','Year','ObserverId')) %>% 
+                        by = c('RouteId','Year','ObserverId')) %>% 
   mutate(NewObserver = replace_na(NewObserver, F)) %>% 
   relocate(RouteId, StateNum, Route, Year,
            SpeciesId, SpeciesCode, SpeciesName, SpeciesSciName,SpeciesTotal,
@@ -54,9 +56,9 @@ BirdHWA_n4 <- left_join(BirdHWA_n3,first_obs,
            ObserverId, ObserverType, ObserverRoute, NewObserver)
 
 BirdHWA_n5 <- BirdHWA_n4 %>% 
-  mutate(YearInfested = replace(YearInfested,!is.finite(YearInfested),0),
-         Infested = replace(Infested,!is.finite(Infested),0),
-         yrhwa = replace(yrhwa,!is.finite(yrhwa),0)
+  mutate(YearInfested = replace(YearInfested, !is.finite(YearInfested), 0),
+         Infested = replace(Infested, !is.finite(Infested), 0),
+         yrhwa = replace(yrhwa, !is.finite(yrhwa), 0)
          )
 
 BirdHWA_2 <- BirdHWA_n5
