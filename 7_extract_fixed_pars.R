7faef0020e1604f39e7ddf00f35dd0ea5dbd012f
br-amaral <br-amaral@users.noreply.github.com>
2021-03-08 09:58:44 -0500

    Create 7_extract_fixed_pars.R
    formulas to extract fixed parameters from results giant tibble

---
+++ 7_extract_fixed_pars.R
@@ -0,0 +1,63 @@
+f_intercept <- function(a){
+  if("(Intercept)" %in% rownames(a)){
+    a[which(rownames(a) == "(Intercept)"), c(1,3,5)]} else {
+      rep(NA,3)
+      }
+}  
+
+f_year_offset <- function(a){
+  if("year_offset" %in% rownames(a)){
+    a[which(rownames(a) == "year_offset"), c(1,3,5)]} else {
+      rep(NA,3)
+      }
+}  
+
+f_infoff <- function(a){
+  if("infoff" %in% rownames(a)){
+    a[which(rownames(a) == "infoff"), c(1,3,5)]} else {   
+      rep(NA,3)
+    }
+}  
+
+f_NewObserver <- function(a){
+  if("NewObserverTRUE" %in% rownames(a)){
+    a[which(rownames(a) == "NewObserverTRUE"), c(1,3,5)]} else {
+      rep(NA,3)
+      }
+}  
+
+f_temp_min_scale <- function(a){
+  if("temp_min_scale" %in% rownames(a)){
+    a[which(rownames(a) == "temp_min_scale"), c(1,3,5)]} else {
+      rep(NA,3)
+      }
+}  
+
+f_year_offset_infoff <- function(a){
+  if("year_offset:infoff" %in% rownames(a)){
+    a[which(rownames(a) == "year_offset:infoff"), c(1,3,5)]} else {
+      rep(NA,3)
+      }
+}  
+
+f_year_offset_temp_min_scale <- function(a){
+  if("year_offset:temp_min_scale" %in% rownames(a)){
+    a[which(rownames(a) == "year_offset:temp_min_scale"), c(1,3,5)]} else {
+      rep(NA,3)
+      }
+}  
+
+f_infoff_temp_min_scale <- function(a){
+  if("infoff:temp_min_scale" %in% rownames(a)){
+    a[which(rownames(a) == "infoff:temp_min_scale"), c(1,3,5)]} else {
+      rep(NA,3)
+      }
+}  
+
+f_year_offset_infoff_temp_min_scale <- function(a){
+  if("year_offset:infoff:temp_min_scale" %in% rownames(a)){
+    a[which(rownames(a) == "year_offset:infoff:temp_min_scale"), c(1,3,5)]} else {
+      rep(NA,3)
+      }
+}  
+
