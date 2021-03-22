d28ca29f779d7c99ed6ae2f832662c1b44c03fff
br-amaral <amaral.bruuna@gmail.com>
2021-03-21 23:59:34 -0400

    Functions to extract and plot predictions

--- 8_params.R
+++ 8_params.R
@@ -15,6 +15,7 @@ RESULT_GLUE <- "{species}/{species}_model{model}_{year}yrs.rds"
 RESULT_GLUE2 <- "/{species}/{species}_model{model}_{year}yrs.rds"
 SUMMARY_RESULT_PATH <- path(RESULT_PATH, "summary_results.rds")
 SPECIES_DATA_PATH <- "data/src/sps_list.csv"
+EACH_SPS_DATA_PATH <- path(DATA_PATH,"species/{species}.rds")
 
 sps_list <- read_csv(SPECIES_DATA_PATH)
 offsets <- seq(2,16,1)
@@ -44,7 +45,7 @@ pb <- progress_bar$new(total = nrow(all_combinations))
 summary_results <- all_combinations %>%
   mutate(result = pmap(., up_res_model))
 
-write_rds(summary_results, SUMMARY_RESULT_PATH)
+#write_rds(summary_results, SUMMARY_RESULT_PATH)
 
 summary_results2 <- summary_results %>%
   mutate(waic_list = map(result, "waic")) %>%
@@ -113,29 +114,104 @@ colnames(summary_results2[11:19])
 "year_offset_temp_min_scale"       
 "infoff_temp_min_scale"            
 "year_offset_infoff_temp_min_scale"
- +
-  A*-6
+
 plot_var(summary_results2, "infoff_temp_min_scale")
 
 # Make predictions and plotting ---------
 # summary_results <- summary_results_MAWA2
 # change all MAWA2 for data object path, and summary_results_MAWA2 for summary_results$results
-species <- "MAWA"
-summary_results2 <- summary_results %>% 
-  filter(species == species) %>% 
-  mutate(pred_path = glue(RESULT_PATH, RESULT_GLUE2),
-         pred_obj = NA)
-#arr <- array(NA, dim = c(14205,6,70))
-preds <- list()
-for(i in 1:nrow(summary_results2)){
-  a <- read_rds(summary_results2$pred_path[i])
-  b <- as.data.frame(a$summary.fitted.values)
-  preds[[i]] <- b
-  rm(a)
-}
+
+plot_pred <- function (yr_type, i, bird2) {
+  if(i > 15) {stop("we do not have that offset!")}
+  if(yr_type == "yrhwa"){
+    predmean <- aggregate(bird2[[i]]$`pred.0.5quant`, list(bird2[[i]]$year_offset), FUN=mean)  ## aggregate medians by year (mean of medians)
+    predUCL<-aggregate(bird2[[i]]$`pred.0.975quant`, list(bird2[[i]]$year_offset), FUN=mean)
+    predLCL<-aggregate(bird2[[i]]$`pred.0.025quant`, list(bird2[[i]]$year_offset), FUN=mean)
+  }
   
+  if(yr_type == "year"){
+    predmean <- aggregate(bird2[[i]]$`pred.0.5quant`, list(bird2[[i]]$Year), FUN=mean)  
+    predUCL<-aggregate(bird2[[i]]$`pred.0.975quant`, list(bird2[[i]]$Year), FUN=mean)
+    predLCL<-aggregate(bird2[[i]]$`pred.0.025quant`, list(bird2[[i]]$Year), FUN=mean)
+  }
+  
+  names(predmean)[names(predmean)=="x"]<-"mean"
+  names(predmean)[names(predmean)=="Group.1"]<-"yrnb"
+  names(predUCL)[names(predUCL)=="x"]<-"UCL"
+  names(predLCL)[names(predLCL)=="x"]<-"LCL"
+  birdpred <- cbind(predmean, predLCL$LCL, predUCL$UCL)
+  colnames(birdpred) <- c("yrnb", "mean", "LCL", "UCL")
+  #freq_key <- as.data.frame(cbind(sort(unique(bird2[[i]]$YearInfested)),table(bird2[[i]]$YearInfested)))
+  #colnames(freq_key) <- c("YearInfested", "Freq_yrinf")
+  #rownames(freq_key) <- NULL
+  #birdpred <- left_join(birdpred, freq_key, by = "YearInfested")
+  
+  LCLmax <- ceiling(max(birdpred$UCL))
+  plot(birdpred$yrnb,birdpred$mean, pch=16, ylim=c(0,LCLmax),
+       xlim = c(1988, 2018),
+       xlab="years since first HWA detection", ylab="birds per route", #main="Segmented Model",
+       cex.lab=1.1, cex.axis=1.1, main = glue("{i+1} year offset")) 
+  lines(x = birdpred$yrnb, y = birdpred$LCL, col="darkgrey", lwd=2)
+  lines(x = birdpred$yrnb, y = birdpred$UCL, col="darkgrey", lwd=2)
+  abline(v = mean(bird2[[i]]$YearInfested[bird2[[i]]$YearInfested != 0 ],
+                  na.rm = TRUE) + i + 1, col = "blue")
+  abline(v = min(bird2[[i]]$YearInfested[bird2[[i]]$YearInfested != 0 ],
+                 na.rm = TRUE)  + i + 1, col = "red")
+}
 
 
+
+species <- "MAWA"
+model <- 3
+yr_type <- "year"   ## year or yrhwa 
+
+inla_pred <- function (species, model, yr_type){
+  summary_results2 <- summary_results %>% 
+    filter(species == species,
+           model == model) %>% 
+    mutate(pred_path = glue(RESULT_PATH, RESULT_GLUE2),
+           pred_obj = NA,
+           sps_tib = glue(EACH_SPS_DATA_PATH))
+  
+  preds <- list()
+  for(i in 1:nrow(summary_results2)){
+    a <- read_rds(summary_results2$pred_path[i])
+    b <- as_tibble(as.data.frame(a$summary.fitted.values))
+    preds[[i]] <- b
+    rm(a)
+  }
+  
+  ind_sps <- list()
+  for(i in 1:nrow(summary_results2)){
+    c <- read_rds(summary_results2$sps_tib[i]) %>% 
+      # year_offset is standardizing yrhwa to the offset (years after infestation to the impact)
+      mutate(year_offset = ifelse(YearInfested != 0, Year - YearInfested - summary_results2$year[i], 0),
+             # infoff: 'infested' route according to the delay in the effect (offset)
+             infoff = ifelse(year_offset <= 0, 0, ifelse(year_offset > 0, 1, NA)))
+    ind_sps[[i]] <- c
+  }
+  
+  bird1 <- Map(cbind, ind_sps, pred = preds)
+  bird2 <- Map(na.omit, bird1)
+  
+  par(mfrow = c(3,5))
+  plot_pred("year", 1, bird2)
+  plot_pred("year", 2, bird2)
+  plot_pred("year", 3, bird2)
+  plot_pred("year", 4, bird2)
+  plot_pred("year", 5, bird2)
+  plot_pred("year", 6, bird2)
+  plot_pred("year", 7, bird2)
+  plot_pred("year", 8, bird2)
+  plot_pred("year", 9, bird2)
+  plot_pred("year", 10, bird2)
+  plot_pred("year", 11, bird2)
+  plot_pred("year", 12, bird2)
+  plot_pred("year", 13, bird2)
+  plot_pred("year", 14, bird2)
+  plot_pred("year", 15, bird2)
+  #print(glue("model {model} predictions for {species}"))
+}
 
 
 
