1a75f1a379bcead9f29b92cfaef698bc5d59ce8d
br-amaral <amaral.bruuna@gmail.com>
2021-04-06 08:28:17 -0400

    Create 8_params.R
    move old script to old folder

---
+++ sandbox/8_params.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(glue)
+library(fs)
+library(progress)
+library(INLA)
+library(tidyselect)
+
+source("5_formulasModels.R")
+
+DATA_PATH <- path("data")
+RESULT_PATH <- path(DATA_PATH, "models_res")
+RESULT_GLUE <- "{species}/{species}_model{model}_{year}yrs.rds"
+RESULT_GLUE2 <- "/{species}/{species}_model{model}_{year}yrs.rds"
+SUMMARY_RESULT_PATH <- path(RESULT_PATH, "summary_results.rds")
+SPECIES_DATA_PATH <- "data/src/sps_list.csv"
+EACH_SPS_DATA_PATH <- path(DATA_PATH,"species/{species}.rds")
+
+sps_list <- read_csv(SPECIES_DATA_PATH)
+offsets <- seq(2,16,1)
+
+all_combinations <- tibble(
+  model = seq_along(formulas),
+  formula = formulas,
+  year = list(offsets),
+  species = list(sps_list$SpeciesCode[5])  # change species here
+) %>%
+  unnest(year) %>%
+  unnest(species) %>%
+  mutate(
+    result_path = path(RESULT_PATH, glue_data(., RESULT_GLUE))
+  )
+
+up_res_model <- function(model, formula, year, species, result_path) {
+  if (exists("pb")) {
+    pb$tick()
+  }
+  result <- read_rds(result_path)
+  summary(result)
+}
+
+pb <- progress_bar$new(total = nrow(all_combinations))
+
+summary_results <- all_combinations %>%
+  mutate(result = pmap(., up_res_model))
+
+#write_rds(summary_results, SUMMARY_RESULT_PATH)
+
