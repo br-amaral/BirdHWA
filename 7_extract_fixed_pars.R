cf26cb2b46cfda33cf875c2c6c15bc88b84d5afe
br-amaral <amaral.bruuna@gmail.com>
2021-04-06 08:27:52 -0400

    Update 7_extract_fixed_pars.R
    different order of the interaction effects

--- 7_extract_fixed_pars.R
+++ 7_extract_fixed_pars.R
@@ -2,14 +2,14 @@ f_intercept <- function(a){
   if("(Intercept)" %in% rownames(a)){
     a[which(rownames(a) == "(Intercept)"), c(1,3,5)]} else {
       rep(NA,3)
-      }
+    }
 }  
 
 f_year_offset <- function(a){
   if("year_offset" %in% rownames(a)){
     a[which(rownames(a) == "year_offset"), c(1,3,5)]} else {
       rep(NA,3)
-      }
+    }
 }  
 
 f_infoff <- function(a){
@@ -23,41 +23,49 @@ f_NewObserver <- function(a){
   if("NewObserverTRUE" %in% rownames(a)){
     a[which(rownames(a) == "NewObserverTRUE"), c(1,3,5)]} else {
       rep(NA,3)
-      }
+    }
 }  
 
 f_temp_min_scale <- function(a){
   if("temp_min_scale" %in% rownames(a)){
     a[which(rownames(a) == "temp_min_scale"), c(1,3,5)]} else {
       rep(NA,3)
-      }
+    }
 }  
 
 f_year_offset_infoff <- function(a){
   if("year_offset:infoff" %in% rownames(a)){
     a[which(rownames(a) == "year_offset:infoff"), c(1,3,5)]} else {
       rep(NA,3)
-      }
+    }
 }  
 
 f_year_offset_temp_min_scale <- function(a){
   if("year_offset:temp_min_scale" %in% rownames(a)){
-    a[which(rownames(a) == "year_offset:temp_min_scale"), c(1,3,5)]} else {
-      rep(NA,3)
-      }
+    a[which(rownames(a) == "year_offset:temp_min_scale"), c(1,3,5)]
+  } else {
+    if("temp_min_scale:year_offset" %in% rownames(a)){
+      a[which(rownames(a) == "temp_min_scale:year_offset"), c(1,3,5)]
+    } else {
+      rep(NA,3)}
+  }
 }  
 
 f_infoff_temp_min_scale <- function(a){
   if("infoff:temp_min_scale" %in% rownames(a)){
     a[which(rownames(a) == "infoff:temp_min_scale"), c(1,3,5)]} else {
       rep(NA,3)
-      }
+    }
 }  
 
 f_year_offset_infoff_temp_min_scale <- function(a){
-  if("year_offset:infoff:temp_min_scale" %in% rownames(a)){
-    a[which(rownames(a) == "year_offset:infoff:temp_min_scale"), c(1,3,5)]} else {
-      rep(NA,3)
-      }
-}  
+  if("year_offset:temp_min_scale" %in% rownames(a)){
+    a[which(rownames(a) == "year_offset:temp_min_scale"), c(1,3,5)]
+  } else {
+    if("temp_min_scale:year_offset" %in% rownames(a)){
+      a[which(rownames(a) == "temp_min_scale:year_offset"), c(1,3,5)]
+    } else {
+      rep(NA,3)}
+  }
+}
 
