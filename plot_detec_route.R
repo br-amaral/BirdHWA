d48a72db0a55cf9ee881d67df9eef8b8b8f6f4d6
br-amaral <amaral.bruuna@gmail.com>
2021-05-02 15:24:19 -0400

    Script to indentify which routes have too many detections (unrealistic)

---
+++ plot_detec_route.R
@@ -0,0 +1,86 @@
+# plot number of detections per route
+
+library(tidyverse)
+library(glue)
+library(ggplot2)
+library(hrbrthemes)
+
+# single species data frames in data/species folder
+SPECIES_DATA_PATH <- "data/src/sps_list.csv"
+SINGLE_SPS <- "data/species"
+BIRD_FILE_PATH <- "data/BirdHWA_2.rds"
+
+BIRDallsps <- readRDS(BIRD_FILE_PATH) %>% 
+  select(Longitude, Latitude) %>% 
+  unique()
+sps_list <- read_csv(SPECIES_DATA_PATH)
+
+plot_data_dist <- function(species, mindec, maxdec) {
+  LOAD_SINGLE_SPS <- glue("{SINGLE_SPS}/{species}.rds")
+  SBIRD <- read_rds(LOAD_SINGLE_SPS)
+  SBIRD2 <- SBIRD %>% 
+    select(SpeciesCode, SpeciesTotal, RouteId, Year, Infested) %>% 
+    filter(!is.na(SpeciesCode)) %>% 
+    filter(SpeciesTotal > mindec) %>% 
+    arrange(RouteId, Year)
+  
+  uni_rou <- unique(SBIRD2$RouteId)
+  len_uni_rou <- length(uni_rou)
+  n_split <- floor(len_uni_rou/50)
+  length(uni_rou) <- prod(dim(matrix(uni_rou, ncol = n_split)))  ## add NA to end of df
+  rout_mat <- matrix(data = uni_rou, nrow = n_split, byrow = T)
+
+  n_splits <- nrow(rout_mat)
+  maxlim <- max(SBIRD2$SpeciesTotal)
+  BIRDs <- vector(mode = "list", length = n_splits)
+  for(i in 1:n_splits){
+    BIRDs[[i]] <- SBIRD2 %>% filter(RouteId %in% rout_mat[i,])
+  }
+  print(map(BIRDs, plot_heat))
+  
+  over_max <- SBIRD %>% 
+    select(SpeciesCode, SpeciesTotal, RouteId, Year, Infested,
+           Latitude, Longitude) %>% 
+    filter(!is.na(SpeciesCode)) %>% 
+    filter(SpeciesTotal > maxdec) %>% 
+    arrange(RouteId, Year)
+  
+  View(over_max)
+  
+  print(
+    
+    ggplot(data = over_max, aes(RouteId, Year, fill= SpeciesTotal)) + 
+      geom_tile() +
+      scale_fill_distiller(palette = "RdPu") +
+      theme_ipsum() +
+      ggtitle(glue(
+        "{BIRDdat$SpeciesCode[1]} - {nrow(over_max)} occasions with over {maxdec} detecs")) +
+      theme(axis.text.x = element_text(angle = 90, 
+                                       vjust = 0.5,
+                                       hjust=1))
+  )
+  
+  par(mfrow=c(1,2))
+  hist(SBIRD$SpeciesTotal)
+  hist(log(SBIRD$SpeciesTotal))
+  
+  par(mfrow=c(1,1))
+  plot(BIRDallsps, col="lightgray")
+  points(over_max$Longitude, over_max$Latitude)
+  
+}
+
+plot_heat <- function(BIRDdat){
+  ggplot(data = BIRDdat, aes(RouteId, Year, fill= SpeciesTotal)) + 
+    geom_tile() +
+    scale_fill_distiller(palette = "Spectral",
+                         limits = c(mindec, maxlim)) +
+    theme_ipsum() +
+    ggtitle(BIRDdat$SpeciesCode[1]) +
+    theme(axis.text.x = element_text(angle = 90, 
+                                     vjust = 0.5,
+                                     hjust=1))
+}
+
+plot_data_dist("ACFL", 0, 30)
+
