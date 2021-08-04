6e36b1cf7a1f0e946ee6047d1e6749accfd91b3a
br-amaral <74007922+br-amaral@users.noreply.github.com>
2021-08-04 14:30:52 -0400

    Update 6_model.R
    -filter data properly - with year_off calculated and them data filtered, data was of different dimensions for each offset. Filter the data, then add the offset.

--- 6_model.R
+++ 6_model.R
@@ -22,50 +22,27 @@ hex.adj <- paste0(getwd(),"/data/hexmap.graph")
 
 offsets <- seq(2,16,1)
 
-run_model <- function(off, BIRDx, formula){
-
-## Create an year offset for that species ------------------  
-   BIRDx <- BIRDx %>% 
-    # year_offset is standardizing yrhwa to the offset (years after infestation to the impact)
-    mutate(year_offset = ifelse(YearInfested != 0, Year - YearInfested + off, 0),
+run_model <- function(offset, BIRDx, formula){
+   ## Create an year offset for that species ------------------  
+  BIRDx <- BIRDx %>%  
+    # remove 20 ears before and after infestation
+    mutate(year_offset = ifelse(YearInfested != 0, Year - YearInfested, 0)) %>% 
+    filter(year_offset > -20 & year_offset < 20) %>% 
+    # Only routes infested for at least 10 years
+    group_by(RouteId) %>% 
+    mutate(max = max(year_offset)) %>%  
+    filter(max > 9) %>% 
+    ungroup() %>% 
+    # year_offset is standardizing yrhwa to the offset (years after infestation to the impact) ADDING THE LAG
+    mutate(year_offset = ifelse(YearInfested != 0, Year - YearInfested + offset, 0),
            # infoff: 'infested' route according to the delay in the effect (offset)
-           infoff = ifelse(year_offset < off, 0, ifelse(year_offset >= off, 1, NA)))
+           infoff = ifelse(year_offset <= 0, 0, ifelse(year_offset > 0, 1, NA)))
+  print(nrow(BIRDx))
+  print(sum(BIRDx$SpeciesTotal))
 
-  rout_notinf <- BIRDx %>% 
-    select(RouteId, Year, YearInfested, Infested) %>% 
-    filter(YearInfested == 0) %>% 
-    distinct() %>% 
-    group_by(RouteId) %>% 
-    mutate(maxYear = max(Year)) %>% 
-    select(RouteId, maxYear) %>% 
-    distinct()
-  
-  ## if a route was never infested, year_offset is 'equal' to the last year it was sampled
-  for(i in 1:nrow(BIRDx)){
-    if(BIRDx$YearInfested[i] == 0){
-      off_noin <- rout_notinf[which(rout_notinf$RouteId == BIRDx$RouteId[i]), 2]
-      BIRDx$year_offset[i] <- BIRDx$Year[i] - as.numeric(off_noin) + off - 1
-    }
-  }
-  ## all filters ----------------------------
-  BIRDx1 <- BIRDx %>% 
-    filter(YearInfested != 0,                               # only routes that were infested at some point
-           year_offset > -20 & year_offset < 20) %>%        # look only +-20 years before/after infestation
-    group_by(RouteId) %>% 
-    group_split()
-  
-  for(i in 1:length(BIRDx1)){                               # look only at routes that were infested for at least 10 years
-    a <- BIRDx1[[i]]
-    maxi <- max(a$year_offset)
-    if(maxi < 10) {BIRDx1[[i]] <- NULL }
-  }
-  
-  BIRDx2_1 <- data.table::rbindlist(BIRDx1) %>% 
-    as_tibble()
-  
-  model <- inla(formula, family= "poisson", data= BIRDx2_1, 
-                control.predictor= list(compute=TRUE), 
-                control.compute= list(waic=TRUE, dic=TRUE, cpo=TRUE))
+  model <- inla(formula, family="poisson", data=BIRDx, 
+                control.predictor=list(compute=TRUE), 
+                control.compute=list(waic=TRUE, dic=TRUE, cpo=TRUE))
   return(model)
 }
 
@@ -88,9 +65,6 @@ run_combinations <- function(species){
     }
   }
 }
-
-#run_combinations("BHVI")
-
 
 lapply(sps_list$SpeciesCode, run_combinations)
 
