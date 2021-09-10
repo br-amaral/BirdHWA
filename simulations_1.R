8629eb1a143d53068cf1def1afc77b4d0f3df97b
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2021-09-10 15:08:33 -0400

    Update simulations_1.R
    - remove values from random effect from model output
    - add the poisson link
    - run the INLA model

--- simulations_1.R
+++ simulations_1.R
@@ -2,9 +2,11 @@ library(lme4)
 library(tidyverse)
 library(dummies)
 library(simglm)
+library(INLA)
+library(glue)
 
 set.seed.(321)
-
+HETH <- readRDS("~/Documents/HETH_model1_2yrs.rds")
 ## SIMULATION 1 - only fied parameters -------------------------------------------------
 #  Page 25 of the book - starting with only fixed parameters and no package 
 
@@ -12,20 +14,16 @@ set.seed.(321)
 reps <- 1
 
 # start with only fixed
-b1 <- 1.2       # year_offset
-b2 <- -2        # infoff
-b3 <- -0.5      # NewObserver
-b4 <- 1.5       # temp_min_scale
+b0 <- 1.12307613
+b1 <- 0.01414918       # year_offset
+b2 <- 0.01414918        # infoff
+b3 <- -0.18482579      # NewObserver
+b4 <- -1.73591647       # temp_min_scale
 # add interaction
-b5 <- -1.35
-b6 <- -0.3
-b7 <- -0.5
-b8 <- -1.1
-# add random
-b9 <- 0.4         # ObserverRoute, model="iid" 
-b10 <- 0.4        # Year, model="iid"
-# spatial effect - so far with no 'map'
-b11 <- 0.4        # hexID, model="bym"
+b5 <- -0.01591541
+b6 <- 0.06844684
+b7 <- 0.18090443
+b8 <- -0.11831990
 
 # matrix to store the estimates
 par.est.pois <- matrix(NA, nrow= reps, ncol = 8) %>% 
@@ -231,8 +229,8 @@ X$ObserverRoute <- ra_eff_obrou
 X$hexId
 
 
-for (i in 1:reps){
-  Y <- exp(1 + b1 * X$year_offset +
+#for (i in 1:reps){
+  lambda <- exp(1 + b1 * X$year_offset +
              b2 * X$infoff +
              b3 * X$NewObserver +
              b4 * X$temp_min_scale +
@@ -288,7 +286,7 @@ for (i in 1:reps){
   par.est.pois$`temp_min_scale:infoff:year_offset_SD`[i] <- sqrt(diag(vcv)[9])
   
   print(i)
-}
+
 
 comp.est <- matrix(NA, ncol = 16, nrow= 2) %>% 
   as.data.frame()
@@ -329,6 +327,7 @@ X <- BIRDtab %>%
          ObserverRoute = as.character(ObserverRoute),
          temp_min_scale = as.numeric(temp_min_scale)
          ) 
+colnames(X)[9] <- "data"
 
 X2 <- X %>% 
   select(year_offset,
@@ -341,103 +340,86 @@ X2 <- X %>%
          temp_min_scale.infoff.year_offset = temp_min_scale * infoff * year_offset) %>% 
   as.matrix()
 
-
-for (i in 1:reps){
-
   Z1 <- model.matrix(~ 0 + X$ObserverRoute)  
   Z2 <- model.matrix(~ 0 + as.character(X$Year))
-
-  b9_ <- rep(b9, length(unique(X$ObserverRoute)))
-  b10_ <- rep(b9, length(unique(X$Year)))
+  Z3 <- model.matrix(~ 0 + as.character(X$hexID))
   
+  g1 <- HETH$summary.random$ObserverRoute[,2]
+  g2 <- HETH$summary.random$Year[,2]
+  g3 <- HETH$summary.random$hexID[,1:2] %>% 
+    as.tibble() %>% 
+    filter(ID %in% X$hexID) %>% 
+    select(mean) %>% 
+    pull(mean)
+
   betas <- c(b1, b2, b3, b4, b5, b6, b7, b8)
     
-  Y <- exp(1 + X2 %*% betas +
-               Z1 %*% b9_ +   #X$ObserverRoute
-               Z2 %*% b10_    #X$Year
+  lambda <- exp(b0 + X2 %*% betas +
+               Z1 %*% g1 +    #X$ObserverRoute
+               Z2 %*% g2 +    #X$Year
+               Z3 %*% g3
            )
   
+  y <- rpois(length(lambda),lambda)
   
-#  Y <- exp(1 + b1 * X2$year_offset +
-#               b2 * X2$infoff +
-#               b3 * X2$NewObserver +
-#               b4 * X2$temp_min_scale +
-#               b5 * X2$year_offset * X$infoff +
-#               b6 * X2$year_offset * X$temp_min_scale +
-#               b7 * X2$temp_min_scale * X$infoff +
-#               b8 * X2$temp_min_scale * X$infoff * X$year_offset +
-#               b9  %*% Z1 + #X$ObserverRoute
-#               b10 %*% Z2 #X$Year
-#  )
+  plot(y)
+
+  X3 <- cbind(as.data.frame(X2), X$ObserverRoute, X$Year, X$hexID)
+  colnames(X3)[9:11] <- c("ObserverRoute", "Year", "hexID")
   
-  coefini <- c(model$coefficients[1:4], "ObserverRoute" = 0, "Year" = 0, model$coefficients[5:9])
-  
-  X3 <- cbind(as.data.frame(X2), X$ObserverRoute, X$Year)
-  colnames(X3)[9:10] <- c("ObserverRoute", "Year")
-  
-  m1 <- glm(Y ~ X3$year_offset + X3$infoff + X3$NewObserver + X3$temp_min_scale + 
-              X3$year_offset * X3$infoff + X3$year_offset * X3$temp_min_scale +
-              X3$temp_min_scale * X3$infoff + X3$temp_min_scale * X3$infoff * X3$year_offset,
+  m1 <- glm(y ~ X$year_offset + X$infoff + X$NewObserver + X$temp_min_scale + 
+              X$year_offset * X$infoff + X$year_offset * X$temp_min_scale +
+              X$temp_min_scale * X$infoff + X$temp_min_scale * X$infoff * X$year_offset,
             family = poisson(link = "log"))
   
-  model <- glmer(Y ~ X3$year_offset + X3$infoff + X3$NewObserver + X3$temp_min_scale + 
-                   X3$year_offset * X3$infoff + X3$year_offset * X3$temp_min_scale +
-                   X3$temp_min_scale * X3$infoff + X3$temp_min_scale * X3$infoff * X3$year_offset +
-                   (1 | X3$ObserverRoute) + 
-                   (1 | X3$Year),
+  m1r <- glmer(y ~ X$year_offset + X$infoff + X$NewObserver + X$temp_min_scale + 
+                   X$year_offset * X$infoff + X$year_offset * X$temp_min_scale +
+                   X$temp_min_scale * X$infoff + X$temp_min_scale * X$infoff * X$year_offset +
+                   (1 | X$ObserverRoute) + 
+                   (1 | X$Year),
                  family = poisson(link = "log"),
-                 start=list(fixef=coef(m1)),
-                 control=glmerControl(nAGQ0initStep=FALSE),
+                 start=list(fixef=coef(m1))
+                 #control=glmerControl(nAGQ0initStep=FALSE),
                  #control = glmerControl(tolPwrss=1e-3),
-                 verbose = 100)
+                 #verbose = 100
+                 )
+  BIRDx <- cbind(y, X3)
+  colnames(BIRDx)[1] <- "SpeciesTotal"
+    
+  formula1 <- SpeciesTotal ~ 1 + 
+  year_offset + 
+  infoff +
+  year_offset : infoff +
+  NewObserver +
+  temp_min_scale +
+  temp_min_scale : year_offset +
+  temp_min_scale : infoff +
+  temp_min_scale : infoff : year_offset +
+  f(ObserverRoute, model="iid") + 
+  f(Year, model="iid") +
+  f(hexID, model="bym", graph=hex.adj, constr=TRUE)  
   
-  vcv <- vcov(model)
+  hex.adj <- paste0("~/Documents/hexmap.graph")
+    
+  m1in <- inla(formula1, family="poisson", data=BIRDx, 
+               control.predictor=list(compute=TRUE), 
+               control.compute=list(waic=TRUE, dic=TRUE, cpo=TRUE))
   
-  par.est.pois$year_offset[i] <- model$coefficients[2]
-  par.est.pois$year_offset_SD[i] <- sqrt(diag(vcv)[2])
   
-  par.est.pois$infoff[i] <- model$coefficients[3]
-  par.est.pois$infoff_SD[i] <- sqrt(diag(vcv)[3])
   
-  par.est.pois$NewObserver[i] <- model$coefficients[4]
-  par.est.pois$NewObserver_SD[i] <- sqrt(diag(vcv)[4])
   
-  par.est.pois$temp_min_scale[i] <- model$coefficients[5]
-  par.est.pois$temp_min_scale_SD[i] <- sqrt(diag(vcv)[5])
   
-  par.est.pois$`year_offset:infoff`[i] <- model$coefficients[6]
-  par.est.pois$`year_offset:infoff_SD`[i] <- sqrt(diag(vcv)[6])
   
-  par.est.pois$`temp_min_scale:year_offset`[i] <- model$coefficients[7]
-  par.est.pois$`temp_min_scale:year_offset_SD`[i] <- sqrt(diag(vcv)[7])
   
-  par.est.pois$`temp_min_scale:infoff`[i] <- model$coefficients[8]
-  par.est.pois$`temp_min_scale:infoff_SD`[i] <- sqrt(diag(vcv)[8])
   
-  par.est.pois$`temp_min_scale:infoff:year_offset`[i] <- model$coefficients[9]
-  par.est.pois$`temp_min_scale:infoff:year_offset_SD`[i] <- sqrt(diag(vcv)[9])
   
-  print(i)
-}
-
-comp.est <- matrix(NA, ncol = 16, nrow= 2) %>% 
-  as.data.frame()
-comp.est[1,] <- colMeans(par.est.pois) 
-comp.est[2,1] <- b1
-comp.est[2,3] <- b2
-comp.est[2,5] <- b3
-comp.est[2,7] <- b4
-comp.est[2,9] <- b5
-comp.est[2,11] <- b6
-comp.est[2,13] <- b7
-comp.est[2,15] <- b8
-colnames(comp.est) <- colnames(par.est.pois)#[c(TRUE, FALSE)]
-rownames(comp.est) <- c("estimated", "simulated")
-comp.est
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
 set.seed(321) 
 
 sim_arguments <- list(
