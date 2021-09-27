1f1f024b0407228504e31f701d40412e4e3b8618
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2021-09-27 17:38:17 -0400

    Update 9_ExtractPars.R
    source from home computer

--- 9_ExtractPars.R
+++ 9_ExtractPars.R
@@ -9,6 +9,7 @@ library(tidyselect)
 spsr <- "RBNU"
 
 source("7_extract_fixed_pars.R")
+#  source("~/Documents/GitHub/BirdHWA/7_extract_fixed_pars.R")
 
 summary_results <- readRDS(glue('data/models_res/{spsr}/summary_results.rds'))
 
