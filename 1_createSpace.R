f0147cca0eff5543da0af9b474d444430346d865
br-amaral <74007922+br-amaral@users.noreply.github.com>
2022-08-04 14:03:01 -0400

    Update 1_createSpace.R
    create and keep a file with RouteId and FIPS info to use to create map (16_map_daset.R can source it)

--- 1_createSpace.R
+++ 1_createSpace.R
@@ -34,8 +34,10 @@ route_hex <- read_csv(ROUTE_DATA_PATH, col_types = cols_only(
                           paste0(RouteId)
                           ))
 
-route_hex <- left_join(route_hex, hexord, by = "FID") %>% 
+route_hex <- left_join(route_hex, hexord, by = "FID")
+
+route_hex2 <- route_hex %>% 
   select(`RouteId`,
          `hexID`)
 
-saveRDS(route_hex, "data/route_hex.rds")
+saveRDS(route_hex2, "data/route_hex.rds")
