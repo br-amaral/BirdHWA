eac58f236fdbcb9db05c2f71a6d36e7714b2049b
br-amaral <74007922+br-amaral@users.noreply.github.com>
2022-08-05 12:52:51 -0400

    Update 17_Map_inf_county.R
    Routes used and fips infested and used in the analysis for GIS map

--- 17_Map_inf_county.R
+++ 17_Map_inf_county.R
@@ -1,5 +1,5 @@
 routes_used_infestime <- 
-  BIRD3 %>% select(RouteId, Year, YearInfested, yrhwa) %>% distinct() %>% mutate(lenInf = 2018 - YearInfested) %>% 
+  BIRD %>% select(RouteId, Year, YearInfested, yrhwa) %>% distinct() %>% mutate(lenInf = 2018 - YearInfested) %>% 
   select(RouteId, lenInf, YearInfested) %>% 
   distinct() #%>% #filter(RouteId %in% c("72013", "72060")) %>% 
   #View()
@@ -10,6 +10,9 @@ for(i in 1:nrow(routes_used_infestime)){
   }
 }
 
+table(routes_used_infestime$lenInf)  # no zeros, perfect!
+
+
 all_routes_data <- readRDS(file = 'data/infestations_2.rds') %>% 
   select(RouteId, YearInfested) %>% 
   distinct() %>% 
@@ -19,7 +22,7 @@ all_routes_data <- readRDS(file = 'data/infestations_2.rds') %>%
 all_routes_data <- left_join(all_routes_data, routes_used_infestime, by = "RouteId")
 
 all_routes_data <- all_routes_data %>% 
-  mutate(YearInfested = ifelse(is.na(YearInfested), 2030, YearInfested))
+  mutate(YearInfested = ifelse(is.na(YearInfested), 2030, YearInfested)) ## 2030 infested but not used in analysis
   
 c(39013, 88009, 88039, 88919)
 
@@ -38,18 +41,31 @@ fips_order <- read_csv("data/fips_order.csv") %>%
   select(FIPS)
 
 fips_route2 <- left_join(fips_order, fips_infes, by = "FIPS") 
+fips_route2$YearInfested2 <- ifelse(is.na(fips_route2$YearInfested), 2030, fips_route2$YearInfested)
 
+routes_order <- read_csv("data/map_routes_order.csv") %>% 
+  select(OBJID,RTENO) %>% 
+  rename(RouteId = RTENO) %>% 
+  mutate(RouteId = as.character(RouteId))
+
+routes_study <- left_join(routes_order, all_routes_data %>% select(RouteId, YearInfested), by ='RouteId')
+routes_study$YearInfested <- ifelse(is.na(routes_study$YearInfested), 2030, routes_study$YearInfested)
+routes_study$YearInfested <- ifelse(routes_study$YearInfested != 2030, 10, routes_study$YearInfested)
+
+ 
 ## add counties with no bbs routes info
-no_route <- read.csv("data/no_route_county.csv") %>% 
-  select(FIPS,YEARFIRST)
+#no_route <- read_csv("data/no_route_county.csv") %>% 
+#  select(FIPS,YEARFIRST)
 
-for(i in 1:nrow(fips_route2)){
+#for(i in 1:nrow(fips_route2)){
 
-    if(is.na(fips_route2$YearInfested[i])){
-      a <- fips_route2$FIPS[i]
-      b <- no_route[which(no_route$FIPS == a),2]
-      fips_route2$YearInfested[i] <- b
-  } else {fips_route2$YearInfested[i] <-fips_route2$YearInfested[i]}
-}
+#    if(is.na(fips_route2$YearInfested[i])){
+#      a <- fips_route2$FIPS[i]
+#      b <- no_route[which(no_route$FIPS == a),2] %>% pull()
+#      fips_route2$YearInfested[i] <- b
+#  } else {fips_route2$YearInfested[i] <-fips_route2$YearInfested[i]}
+#}
 
 write_csv(fips_route2, file = "data/fips_route2.csv")
+write_csv(routes_study, file = "data/routes_study.csv")
+
