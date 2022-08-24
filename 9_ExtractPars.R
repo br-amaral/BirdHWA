b28ae08a20dcb71cac5386fdcafe05c0170dd290
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2022-08-24 15:51:12 -0400

    add information about code add a progress bar remove unused code

--- 9_ExtractPars.R
+++ 9_ExtractPars.R
@@ -1,18 +1,53 @@
+# 9_ExtractPars.R --------------------------------
+# extract coefficient estimates and WAIC information from tibble column to different
+#    columns in a tibble
+# select the best model and offsets for each species (plot)
+# 
+# INPUT
+#   data/src/sps_list.csv: list of species analised
+#   7_extract_fixed_pars.R: sourcing functions to extract parameters from list
+#   data/models_resnew/{spsr}/summary_results.rds: results of all models and offsets for a species
+#
+# OUTPUT
+#   data/models_resnew/{species}/summary_results2.rds: coefficient of all models and offsets for a species
+
+
+# Load packages ------------------------------
 library(tidyverse)
 library(glue)
 library(fs)
 library(INLA)
 library(tidyselect)
 
-### choose species here! species list is: 
-
+# Load functions that extract parameters from list on a tibble column
 source("~/Documents/GitHub/BirdHWA/7_extract_fixed_pars.R")
 
+# Load species list 
 SPECIES_DATA_PATH <- "data/src/sps_list.csv"
 sps_list <- read_csv(SPECIES_DATA_PATH)
+pb <- progress_bar$new(total = nrow(sps_list))
 
-for(i in 1:nrow(sps_list)){
-  spsr <- species <-  pull(sps_list[i,])
+pars_models_FUNC <- function(i) {
+  summary_results2 %>%
+    rowwise() %>%
+    mutate(intercept = as.numeric(intercept[[i]]),
+           year_offset = year_offset[[i]],
+           infoff = infoff[[i]],
+           NewObserver = NewObserver[[i]],
+           temp_min_scale = temp_min_scale[[i]],
+           year_offset_infoff = year_offset_infoff[[i]],
+           year_offset_temp_min_scale = year_offset_temp_min_scale[[i]],
+           infoff_temp_min_scale = infoff_temp_min_scale[[i]],
+           year_offset_infoff_temp_min_scale = year_offset_infoff_temp_min_scale[[i]],
+           esti_type = ifelse(i == 1, "mean", ifelse(i == 2, "low", "up"))) %>% 
+    select(-c(result_path, result, waic_list, waicNull, fixed))
+}
+
+for(k in 1:nrow(sps_list)){
+  if (exists("pb")) {
+    pb$tick()
+  }
+  spsr <- species <- pull(sps_list[k,])
   summary_results <- readRDS(glue('data/models_resnew/{spsr}/summary_results.rds'))
   
   summary_results2 <- summary_results %>%
@@ -40,101 +75,17 @@ for(i in 1:nrow(sps_list)){
            infoff_temp_min_scale = map(fixed, f_infoff_temp_min_scale),
            year_offset_infoff_temp_min_scale = map(fixed, f_year_offset_infoff_temp_min_scale)) 
   
-  pars_models_FUNC <- function(i) {
-    summary_results2 %>%
-      rowwise() %>%
-      mutate(intercept = as.numeric(intercept[[i]]),
-             year_offset = year_offset[[i]],
-             infoff = infoff[[i]],
-             NewObserver = NewObserver[[i]],
-             temp_min_scale = temp_min_scale[[i]],
-             year_offset_infoff = year_offset_infoff[[i]],
-             year_offset_temp_min_scale = year_offset_temp_min_scale[[i]],
-             infoff_temp_min_scale = infoff_temp_min_scale[[i]],
-             year_offset_infoff_temp_min_scale = year_offset_infoff_temp_min_scale[[i]],
-             esti_type = ifelse(i == 1, "mean", ifelse(i == 2, "low", "up"))) %>% 
-      select(-c(result_path, result, waic_list, waicNull, fixed))
-  }
-  
   pars_models <- as_tibble(rbind(pars_models_FUNC(1),
                                  pars_models_FUNC(2),
                                  pars_models_FUNC(3)))
   
   (waic_best <- summary_results2[which(summary_results2$waic == min(summary_results2$waic)),1:4])
+  ifelse(k == 1, waic_best2 <- waic_best, waic_best2 <- rbind(waic_best2, waic_best))
   
   write_rds(summary_results2, file = glue("data/models_resnew/{species}/summary_results2.rds"))
+  write_rds(waic_best2, file = "data/waicbest.rds")
   rm(summary_results2)
   rm(summary_results)
   rm(species)
   rm(spsr)
 }
-
-year_ <- waic_best$year[1]
-mod_ <- waic_best$model[1]
-nacol <- c("mean", "low", "up","par")
-par_tib <- pars_models %>% 
-  filter(model == mod_,
-         year == year_,
-         species == spsr) %>% 
-  mutate(WAIC = waic) %>% 
-  select(-c(model, formula, year, species, waic)) %>% 
-  relocate(esti_type) %>% 
-  t() %>% 
-  cbind(rownames(.)) %>% 
-  as_tibble() %>% 
-  rename_with(~nacol) %>% 
-  mutate(mean = as.double(mean),
-         low = as.double(low),
-         up = as.double(up)) %>% 
-  relocate(par)
-(par_tib <- par_tib[-1,])
-
-# waic plot   ---------------------
-ggplot(aes(year, 
-           #jitter(waic, amount = 0.5),
-           waic,
-           group=model, color=factor(model)), data = summary_results2) +
-  geom_line(alpha=0.5) +
-  geom_point(alpha=0.5)
-
-# ploting parameters  -----------------
-model_names <- paste(rep("Model",10), seq(1,10), sep= " ")
-
-plot_var <- function(my_tibble, variable1) {
-  my_tibble %>% 
-    select(all_of(variable1)) %>% 
-    unlist() %>% 
-    matrix(., ncol = 3, byrow = T) %>% 
-    as_tibble() %>% 
-    rename(mean = V1, low = V2, up = V3) %>% 
-    bind_cols(summary_results2[,c(4,1,3)], .) %>%
-    mutate(model_name = as.character(glue("Model {model}"))) %>% 
-    group_by(model) %>% 
-    transform(model_name = factor(model_name, levels= model_names)) %>% 
-    ggplot(aes(year, mean)) +
-    geom_hline(aes(yintercept = 0, colour="red"),
-               show.legend = FALSE) +
-    geom_errorbar(aes(ymin= low, ymax= up),
-                  colour = "grey65") +
-    geom_point() +
-    facet_wrap(~ model_name, ncol = 5) + 
-    theme_bw() +
-    labs(title = variable1) +
-    theme(plot.title = element_text(hjust = 0.5))
-}
-
-colnames(summary_results2[11:19])
-
-"intercept_change"                 
-"year_offset"                      
-"infoff"                           
-"NewObserver"                      
-"temp_min_scale"                   
-"year_offset_infoff"               
-"year_offset_temp_min_scale"       
-"infoff_temp_min_scale"            
-"year_offset_infoff_temp_min_scale"
-
-plot_var(summary_results2, "infoff")
-plot_var(summary_results2, "year_offset_infoff")
-plot_var(summary_results2, "year_offset_infoff_temp_min_scale")
