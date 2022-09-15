3aec9c7734e2002ffaf2a4de162c511a8eef68be
Bruna Amaral <74007922+br-amaral@users.noreply.github.com>
2022-09-15 14:11:38 -0400

    organize yaxis

--- 11_PlotPercent.R
+++ 11_PlotPercent.R
@@ -42,25 +42,35 @@ for(i in seq(from=1, to=nrow(sps_preds2), by=2)) {
 }
 
 sps_preds3 <- sps_preds2 %>% 
-  filter(HWA == "infest")   ## get only a copy from prop
+  filter(HWA == "infest") %>%    ## get only a copy from prop
+  unite(sps_temp, c(species,temp), remove = FALSE) 
 
 order <- rev(c("ACFL", "BHVI", "BLBW", "BTNW", "HETH", "MAWA", "RBNU",
                "BLJA", "CERW", "EAPH", "REVI", "SCTA", "WBNU", "WOTH"))
 
-sps_preds3$species <- factor(sps_preds3$species, levels = order)
+sps_preds3$species <- factor(sps_preds3$species, levels = order) 
 
-svg(glue("Figures/percent_plot.svg"), 
+facs <- sps_preds3$sps_temp
+sps_preds3$sps_temp <- factor(sps_preds3$sps_temp, 
+                                levels = c("WOTH_t1", "WOTH_t2", "WOTH_t3", "WEWA_t1", "WEWA_t2", "WEWA_t3", "WBNU_t1", "WBNU_t2", "WBNU_t3",
+                                           "SCTA_t1", "SCTA_t2", "SCTA_t3", "REVI_t1", "REVI_t2", "REVI_t3", "EAPH_t1", "EAPH_t2", "EAPH_t3",
+                                           "CERW_t1", "CERW_t2", "CERW_t3", "BLJA_t1", "BLJA_t2", "BLJA_t3", "RBNU_t1", "RBNU_t2", "RBNU_t3",
+                                           "MAWA_t1", "MAWA_t2", "MAWA_t3", "HETH_t1", "HETH_t2", "HETH_t3", "BTNW_t1", "BTNW_t2", "BTNW_t3",
+                                           "BLBW_t1", "BLBW_t2", "BLBW_t3", "BHVI_t1", "BHVI_t2", "BHVI_t3", "ACFL_t1", "ACFL_t2", "ACFL_t3"))
+
+svg(glue("Figures/Fig3/percent_plot.svg"), 
     width = 8, height = 9)
-ggplot(data = sps_preds3, aes(y= species, x = prop)) +
+ggplot(data = sps_preds3, aes(y= sps_temp, x = prop)) +
   geom_vline(xintercept = 0,
              col = "gray43",
-             linetype = "dotted",
              size = 0.8) +
   geom_vline(xintercept = -0.3,
              col = "gray43",
+             linetype = "dotted",
              size = 0.8) +
   geom_vline(xintercept = 0.3,
              col = "gray43",
+             linetype = "dotted",
              size = 0.8) +
   geom_point(aes(shape = temp, color = temp), size = 3) +
   theme_bw() +
@@ -68,14 +78,9 @@ ggplot(data = sps_preds3, aes(y= species, x = prop)) +
         panel.grid.minor = element_blank(),
         axis.text = element_text(size = 12),
         #legend.title = element_blank(),
-        legend.position = "right",
-        legend.justification = "right",
-        legend.text=element_text(size=12),
-        legend.title=element_text(size=12),
-        #legend.margin=margin(0,0,0,0),
-        #legend.box.margin=margin(-5,0,-5,-7),
-        axis.title.x = element_blank(),
-        #axis.title.y = element_blank(),
+        legend.position = "none",
+        #axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         legend.title.align = 0.5) +
   scale_x_continuous(breaks = seq(-3, 1, 1),
                      limits = c(-3, 1, 1)) +
@@ -86,6 +91,10 @@ ggplot(data = sps_preds3, aes(y= species, x = prop)) +
                                 "t2" = "0.5",
                                 "t3" = "0.8"),
                      name = "Temperature\nQuantiles") +
-  ylab("Log(Not infested route \n      Infested route)") 
+  xlab("Log(Not infested route \n      Infested route)") +
+  scale_y_discrete(labels = c("\n", "WOTH", "\n", "\n", "REVI", "\n", "\n", "WBNU", "\n", "\n", "SCTA", "\n",
+                              "\n", "EAPH", "\n", "\n", "CERW", "\n", "\n", "BLJA", "\n", "\n", "RBNU", "\n",
+                              "\n", "MAWA", "\n", "\n", "HETH", "\n", "\n", "BTNW", "\n", "\n", "BLBW", "\n",
+                              "\n", "BHVI", "\n", "\n", "ACFL", "\n")) 
 
 dev.off()
